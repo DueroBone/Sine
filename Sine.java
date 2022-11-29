f599e5432ee2995d77d4754423fcdaee143f3c40
DueroBone <90635383+DueroBone@users.noreply.github.com>
2022-11-28 19:38:52 -0500

    Declaring variables

--- Sine.java
+++ Sine.java
@@ -1,11 +1,13 @@
 public class Sine {
     public static void main(String[] args) {
+      int samplesTest = 0; //Adjustable
       int pi = 3.141592653589793238;
       int pi2 = pi/2;
-      int samplesTest = 0; //Adjustable
       int samplesExp = 20;
+      int x = 0;
+      int exp = 0;
     }
-    public static int Test(int input) {
+    public static int compSin(int input) {
       input = input + 1;
       return input;
     }
