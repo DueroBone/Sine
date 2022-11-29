318970b6761399c9f2cfc06f2c6d478f1c212ad1
DueroBone <90635383+DueroBone@users.noreply.github.com>
2022-11-28 19:31:40 -0500

    Add files via upload

---
+++ Sine.java
@@ -0,0 +1,12 @@
+public class Sine {
+    public static void main(String[] args) {
+      int pi = 3.141592653589793238;
+      int pi2 = pi/2;
+      int samplesTest = 0; //Adjustable
+      int samplesExp = 20;
+    }
+    public static int Test(int input) {
+      input = input + 1;
+      return input;
+    }
+  }
