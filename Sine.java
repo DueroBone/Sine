d5630296dcd4caabff2f25d85a38dc09c259dea6
DueroBone <90635383+DueroBone@users.noreply.github.com>
2022-11-30 20:06:31 -0500

    Making variables global

--- Sine.java
+++ Sine.java
@@ -1,14 +1,15 @@
 public class Sine {
-    public static void main(String[] args) {
-      int samplesTest = 0; //Adjustable
-      int pi = 3.141592653589793238;
-      int pi2 = pi/2;
-      int samplesExp = 20;
-      int x = 0;
-      int exp = 0;
-    }
-    public static int compSin(int input) {
-      input = input + 1;
-      return input;
-    }
+int samplesTest = 0; //Adjustable
+int pi = 3.141592653589793238;
+int pi2 = pi/2;
+int samplesExp = 20;
+int x = 0;
+int exp = 0;
+  public static void main(String[] args) {
+    
   }
+  public static int compSin(int input) {
+    input = input + 1;
+    return input;
+  }
+}
