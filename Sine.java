83f43ff1ddb7b213bf9c6bc756fa4dda130ca97f
BuildTools <dueromoods@gmail.com>
2022-11-30 22:09:54 -0500

    Forgot the indention

--- Sine.java
+++ Sine.java
@@ -1,16 +1,16 @@
 import java.lang.Math;
 public class Sine {
-static double samplesTest = 0; //Adjustable
-static double pi = 3.141592653589793238;
-static double pi2 = pi/2;
-static double samplesExp = 20;
-static double x = 0;
-static double exp = 0;
-  public static void main(String[] args) {
-    
-  }
-  public static double compSin(double input) {
-    double n1 = ((Math.pow(((x%pi)-pi2),exp)/Math.pow(pi2,exp))*-1) + 1;
-    return n1;
-  }
+  static double samplesTest = 0; //Adjustable
+  static double pi = 3.141592653589793238;
+  static double pi2 = pi/2;
+  static double samplesExp = 20;
+  static double x = 0;
+  static double exp = 0;
+    public static void main(String[] args) {
+      
+    }
+    public static double compSin(double input) {
+      double n1 = ((Math.pow(((x%pi)-pi2),exp)/Math.pow(pi2,exp))*-1) + 1;
+      return n1;
+    }
 }
