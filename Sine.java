f165445ce018407374591f0a18f70e0efab3351a
BuildTools <dueromoods@gmail.com>
2022-11-30 21:54:55 -0500

    continueing

--- Sine.java
+++ Sine.java
@@ -1,15 +1,17 @@
+
 public class Sine {
-int samplesTest = 0; //Adjustable
-int pi = 3.141592653589793238;
-int pi2 = pi/2;
-int samplesExp = 20;
-int x = 0;
-int exp = 0;
+import math
+static double samplesTest = 0; //Adjustable
+static double pi = 3.141592653589793238;
+static double pi2 = pi/2;
+static double samplesExp = 20;
+static double x = 0;
+static double exp = 0;
   public static void main(String[] args) {
     
   }
-  public static int compSin(int input) {
-    input = input + 1;
+  public static double compSin(double input) {
+    double n1 = ((Math.pow(((x%pi)-pi2),exp)/(pi2**exp)) * -1) + 1;
     return input;
   }
 }
