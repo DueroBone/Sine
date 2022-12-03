c628525ea992ee6698a321d26f0e0e705d540bbe
BuildTools <dueromoods@gmail.com>
2022-12-03 15:15:30 -0500

    Lots of work

--- Sine.java
+++ Sine.java
@@ -1,16 +1,42 @@
 import java.lang.Math;
+//import java.lang.reflect.Array;
 public class Sine {
-  static double samplesTest = 0; //Adjustable
+  static double samplesTest = 100; //Adjustable
   static double pi = 3.141592653589793238;
   static double pi2 = pi/2;
-  static double samplesExp = 20;
-  static double x = 0;
-  static double exp = 0;
+  static double samplesExponent = 20;
+  //static double x = 0;
+  static double exponent = 0;
     public static void main(String[] args) {
-      
+      exponent = 2;
+      System.out.println(compExponent());
     }
-    public static double compSin(double input) {
-      double n1 = ((Math.pow(((x%pi)-pi2),exp)/Math.pow(pi2,exp))*-1) + 1;
+
+
+    public static double compSin(double x) {
+      double n1 = ((Math.pow(((x%pi)-pi2),exponent)/Math.pow(pi2,exponent))*-1) + 1;
       return n1;
+    }
+
+    public static double findAverageOfArray(double input[]){
+      double total = 0;
+      for (int i = 0; i < input.length; i=i+1){
+        total = total + input[i];
+        System.out.println("Total = " + input[i] + " " + i);
+      }
+      total = total/input.length;
+      return total;
+    }
+
+    public static double compExponent(){
+      double xIncrement = pi/samplesTest;
+      double differences[] = new double[(int) samplesTest];
+      double x = 0;
+      for (int i = 0; i<samplesTest; i++) {
+        x = + xIncrement;
+        differences[i] = Math.abs(compSin(x) - Math.sin(x));
+        System.out.println("X = " + x + "  |  Sine = " + compSin(x));
+      }
+      return findAverageOfArray(differences);
     }
 }
---
+++ test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args) {
+        String array[] = {"a", "b", "c"};
+        array[3] = "d";
+        System.out.println(array[2]);
+    }
+}
