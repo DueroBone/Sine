95b6ff122a161a0ecd9cdd896b9564482ffab8a6
BuildTools <dueromoods@gmail.com>
2022-12-15 22:08:29 -0500

    BigDecimal has failed me!

--- Sine.java
+++ Sine.java
@@ -1,23 +1,41 @@
 import java.lang.Math;
 import java.lang.reflect.Array;
+import java.math.BigDecimal;
+import java.math.MathContext;
 public class Sine {
-  static int samplesTest = 1000; //Adjustable   <=100000000
-  static double pi = 3.141592653589793238;
-  static double pi2 = pi/2;
+  static BigDecimal samplesTest = new BigDecimal(100); //Adjustable   <=100000000
+  static BigDecimal pi = new BigDecimal(Math.PI);
+  static BigDecimal halfPi = pi.divide(new BigDecimal(2));
   static int samplesExponent = 20;
-  //static double x = 0;
-  static double exponent = 0;
+  //static BigDecimal exponent = new BigDecimal("0.0");
     public static void main(String[] args) {
-      exponent = 1;
-      System.out.println(computeDigit());
+      BigDecimal var = new BigDecimal(1.88);
+      System.out.println(computeSine(var, var));
+      //System.out.println(computeExponent(var));
     }
 
-
-    public static double computeSine(double x) {
-      double n1 = ((Math.pow(((x%pi)-pi2),exponent)/Math.pow(pi2,exponent))*-1) + 1;
+    //This doesn't work, becuase the BigDecimal is unable to be raised to the power of another BigDecimal. At that point, might as well just do it with doubles...
+    public static BigDecimal computeSine(BigDecimal x, BigDecimal exponent) {
+      BigDecimal n2 = halfPi.pow(exponent.intValueExact(), MathContext.DECIMAL128);
+      BigDecimal n1 = x.remainder(pi).subtract(halfPi).pow(exponent.intValueExact(), MathContext.DECIMAL128);
+      n1 = n1.divide(n2, MathContext.DECIMAL128);
+      n1 = n1.multiply(new BigDecimal("-1")).add(new BigDecimal("1"));
       return n1;
     }
-
+    
+    public static double computeExponent(BigDecimal exponent) {
+      BigDecimal xIncrement = pi.divide(samplesTest);
+      double differences[] = new double[samplesTest.intValue()];
+      BigDecimal x = new BigDecimal("0");
+      for (int i=0; i<samplesTest.intValue(); i++) {
+        x = x.add(xIncrement);
+        differences[i] = (computeSine(x, exponent).subtract(new BigDecimal(Math.sin(x.doubleValue())))).abs().doubleValue();
+        System.out.println(differences[i]);
+      }
+      System.out.println("");
+      return findAverageOfArray(differences);
+    }
+    
     public static double findAverageOfArray(double input[]) {
       double total = 0;
       for (int i = 0; i < input.length; i=i+1){
@@ -26,7 +44,7 @@ public class Sine {
       total = total/input.length;
       return total;
     }
-
+    /*
     public static double findHighestOfArray(double input[]) {
       double highest = 0;
       for (int i=0; i<input.length; i++) {
@@ -37,27 +55,15 @@ public class Sine {
       return highest;
     }
 
-    public static double computeExponent() {
-      double xIncrement = pi/samplesTest;
-      double differences[] = new double[samplesTest];
-      double x = 0;
-      for (int i=0; i<samplesTest; i++) {
-        x = x + xIncrement;
-        differences[i] = Math.abs(computeSine(x) - Math.sin(x));
-      }
-      return findAverageOfArray(differences);
-    }
+    /*
     public static double computeDigit() {
       double accuracy[] = new double[samplesExponent];
       double exponentIncrement = 1.0/samplesExponent;
       for (int i=0; i<samplesExponent; i++){
         accuracy[i] = computeExponent();
         exponent = exponent + exponentIncrement;
-        //System.out.println(exponent + " | " + accuracy[i]);
       }
       return findHighestOfArray(accuracy);
     }
-    public static void zoomExponent() {
-
-    }
+    */
 }
