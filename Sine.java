b2a46f12da34e82408910c1e34803c0f727a5df1
BuildTools <dueromoods@gmail.com>
2022-12-17 15:50:34 -0500

    Working now

--- Sine.java
+++ Sine.java
@@ -1,50 +1,51 @@
 import java.lang.Math;
-import java.lang.reflect.Array;
-import java.math.BigDecimal;
-import java.math.MathContext;
 public class Sine {
-  static BigDecimal samplesTest = new BigDecimal(100); //Adjustable   <=100000000
-  static BigDecimal pi = new BigDecimal(Math.PI);
-  static BigDecimal halfPi = pi.divide(new BigDecimal(2));
-  static int samplesExponent = 20;
-  //static BigDecimal exponent = new BigDecimal("0.0");
+  static int samplesTest = 10000; //Adjustable   <=100000000
+  static int maxRepeats = 10000;
+  static double pi = Math.PI;
+  static double pi2 = pi/2;
+  static boolean debugMode = false;
     public static void main(String[] args) {
-      BigDecimal var = new BigDecimal(1.88);
-      System.out.println(computeSine(var, var));
-      //System.out.println(computeExponent(var));
+      GradientAscent(1);
     }
 
-    //This doesn't work, becuase the BigDecimal is unable to be raised to the power of another BigDecimal. At that point, might as well just do it with doubles...
-    public static BigDecimal computeSine(BigDecimal x, BigDecimal exponent) {
-      BigDecimal n2 = halfPi.pow(exponent.intValueExact(), MathContext.DECIMAL128);
-      BigDecimal n1 = x.remainder(pi).subtract(halfPi).pow(exponent.intValueExact(), MathContext.DECIMAL128);
-      n1 = n1.divide(n2, MathContext.DECIMAL128);
-      n1 = n1.multiply(new BigDecimal("-1")).add(new BigDecimal("1"));
+    public static double computeSine(double x, double exponent) {
+      double n1 = ((Math.pow(((x%pi)-pi2),exponent)/Math.pow(pi2,exponent))*-1) + 1;
+      if (debugMode) {
+        System.out.println("");
+        System.out.print("Custom sine of x: ");
+        System.out.print(x);
+        System.out.print(", exponent is: ");
+        System.out.print(exponent);
+        System.out.print(", result is: ");
+        System.out.print(n1);
+      }
       return n1;
     }
-    
-    public static double computeExponent(BigDecimal exponent) {
-      BigDecimal xIncrement = pi.divide(samplesTest);
-      double differences[] = new double[samplesTest.intValue()];
-      BigDecimal x = new BigDecimal("0");
-      for (int i=0; i<samplesTest.intValue(); i++) {
-        x = x.add(xIncrement);
-        differences[i] = (computeSine(x, exponent).subtract(new BigDecimal(Math.sin(x.doubleValue())))).abs().doubleValue();
-        System.out.println(differences[i]);
+
+    public static double computeExponent(double exponent) {
+      double xIncrement = pi/samplesTest;
+      double differences[] = new double[samplesTest];
+      double x = 0;
+      for (int i=0; i<samplesTest; i++) {
+        x = x + xIncrement;
+        differences[i] = Math.abs(computeSine(x, exponent) - Math.sin(x));
       }
-      System.out.println("");
       return findAverageOfArray(differences);
     }
-    
+
     public static double findAverageOfArray(double input[]) {
       double total = 0;
       for (int i = 0; i < input.length; i=i+1){
-        total = total + input[i];
+        double a = input[i];
+        if (a == a) {
+         total = total + input[i];
+        }
       }
       total = total/input.length;
       return total;
     }
-    /*
+
     public static double findHighestOfArray(double input[]) {
       double highest = 0;
       for (int i=0; i<input.length; i++) {
@@ -55,15 +56,45 @@ public class Sine {
       return highest;
     }
 
-    /*
-    public static double computeDigit() {
-      double accuracy[] = new double[samplesExponent];
-      double exponentIncrement = 1.0/samplesExponent;
-      for (int i=0; i<samplesExponent; i++){
-        accuracy[i] = computeExponent();
-        exponent = exponent + exponentIncrement;
+    public static void GradientAscent(double startingExp) {
+      double exponent = startingExp;
+      double expIncrement = 0.01;
+      double expTesting = exponent;
+      double result1;
+      double result2;
+      boolean wasGoingUp = true;
+      boolean isGoingUp = true;
+      for (int i=0; i<maxRepeats; i=i+1) {
+        result1 = computeExponent(expTesting);
+        if (wasGoingUp) {
+          expTesting = exponent - expIncrement;
+        } else {
+          expTesting = exponent + expIncrement;
+        }
+        result2 = computeExponent(expTesting);
+        if (exponent == expTesting) {
+          i = maxRepeats;
+        }
+        if (result1 < result2) { //if currently going up
+          isGoingUp = true;
+          if (wasGoingUp) {
+            exponent = expTesting;
+          } else { //was going down
+            expIncrement = expIncrement/2;
+            exponent = expTesting + expIncrement;
+          }
+          wasGoingUp = true;
+        } else { //if currently going down
+          isGoingUp = false;
+          if (wasGoingUp) {
+            expIncrement = expIncrement/2;
+            exponent = expTesting - expIncrement;
+          } else { //was going down
+            exponent = expTesting;
+          }
+          wasGoingUp = false;
+        }
       }
-      return findHighestOfArray(accuracy);
+      System.out.println("Best exponent is between: " + exponent + " and " + expTesting);
     }
-    */
 }
